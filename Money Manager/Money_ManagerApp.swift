8a27117ae11eab443b7895dbab71f3866b99f25b
Anthony Aniobi <anthonyaniobi198@gmail.com>
2023-04-01 06:52:03 +0100

    Initial Commit

---
+++ Money Manager/Money_ManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Money_ManagerApp.swift
+//  Money Manager
+//
+//  Created by Anthony Aniobi on 01/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct Money_ManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
